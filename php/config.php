e70bfeb1d2e872cb712273ca6a8e8fe29cc3827b
caloloc2 <calolomino@gmail.com>
2018-06-25 18:51:50 -0500

    actualiza

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
 define("HOSTNAME", "localhost");// Nombre del host (localhost aws instance)
 define("USERNAME", "root"); // Nombre del usuario
-define("PASSWORD", "385402292Mica_02"); // Contraseña
+define("PASSWORD", "root"); // Contraseña
 define("DATABASE", "neumaticos"); // Nombre de la base de datos
